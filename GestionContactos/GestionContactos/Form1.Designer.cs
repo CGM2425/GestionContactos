4858ea9ff2276302030fecd2503f3d15f8a6f815
CLAUDIA GARCÍA MARTÍNEZ <clagarmar13@alu.edu.gva.es>
2025-01-21 19:46:13 +0100

    Boton añadir contacto

---
+++ GestionContactos/GestionContactos/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GestionContactos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.Location = new System.Drawing.Point(73, 48);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(115, 32);
+            this.btnAñadir.TabIndex = 0;
+            this.btnAñadir.Text = "Añadir contacto";
+            this.btnAñadir.UseVisualStyleBackColor = true;
+            this.btnAñadir.Click += new System.EventHandler(this.btnAñadir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 279);
+            this.Controls.Add(this.btnAñadir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAñadir;
+    }
+}
+
---
+++ GestionContactos/GestionContactos/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace GestionContactos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int Tam = 10;
+        string[] vNombre = new string[Tam];
+        string[] vTlf = new string[Tam];
+
+        void LeerNombre(string[] v, string[] vTelefono)
+        {
+            for (int i = 0; i < vNombre.Length; i++)
+            {
+                v[i] = Interaction.InputBox("Introduzca un nombre: ");
+                vTelefono[i] = Interaction.InputBox("Introduzca un Telefono: ");
+            }
+        }
+        private void btnAñadir_Click(object sender, EventArgs e)
+        {
+            LeerNombre(vNombre, vTlf);
+        }
+    }
+}
